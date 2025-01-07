7d0ad7e59cb7c51b596d89c9bbc562d0a5a55c0d
liililiil <yuandong.wang@qq.com>
2025-01-07 17:25:45 +0800

    develop

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { onMounted, ref, watch,nextTick } from 'vue'
+import { onMounted, ref, watch, nextTick } from 'vue'
 import * as monaco from 'monaco-editor'
 import LanguageSelector from '@/components/LanguageSelector.vue'
 import CircularProgress from '@/components/CircularProgress.vue'
-import hljs from "highlight.js";
+import hljs from 'highlight.js'
 
 const ref_questionEditorContainer = ref(null)
 const ref_answerEditorContainer = ref(null)
@@ -150,7 +150,7 @@ watch(answerLanguage, (value) => {
 })
 init()
 
- ///
+///
 
 // 控制台日志数组
 const logs = ref([])
@@ -197,10 +197,10 @@ setInterval(() => {
 }, 5000) // 每隔5秒输出日志
 //
 const code = ref('{\n' +
-    '    "Email": "string",\n' +
-    '    "Wxid": "string"\n' +
-    '}')
-const err = ref();
+  '    "Email": "string",\n' +
+  '    "Wxid": "string"\n' +
+  '}')
+const err = ref()
 // 初始化动态的 data
 const data = ref(`{
 import heapq
@@ -263,16 +263,16 @@ watch(displayedCode, () => {
 
 <template>
   <div class="app">
-<!--    <div class="app-header">-->
-<!--      <div class="logo">-->
-<!--        logo-->
-<!--      </div>-->
-<!--      <div class="buttons">-->
-<!--        <div class="logo">-->
-<!--          iDebug-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
+    <!--    <div class="app-header">-->
+    <!--      <div class="logo">-->
+    <!--        logo-->
+    <!--      </div>-->
+    <!--      <div class="buttons">-->
+    <!--        <div class="logo">-->
+    <!--          iDebug-->
+    <!--        </div>-->
+    <!--      </div>-->
+    <!--    </div>-->
     <div class="app-content">
       <div class="section-block">
         <h1>欢迎来到iDebug,有什么可以帮忙的？</h1>
@@ -291,7 +291,12 @@ watch(displayedCode, () => {
           <h2>Design Code Input:</h2>
           <LanguageSelector v-model="answerLanguage" />
           <div class="button-item run-btn" @click="runCode">
-            <svg t="1736214690658" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2516" width="20" height="20"><path d="M396.5 661.9V110c0-6.1 7.5-9 11.6-4.6l366.6 394.3c2.4 2.6 2.4 6.6 0 9.1L408.1 903.2c-4.1 4.5-11.6 1.5-11.6-4.6V661.9z" fill="#67B47A" p-id="2517"></path></svg>
+            <svg t="1736214690658" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                 p-id="2516" width="20" height="20">
+              <path
+                d="M396.5 661.9V110c0-6.1 7.5-9 11.6-4.6l366.6 394.3c2.4 2.6 2.4 6.6 0 9.1L408.1 903.2c-4.1 4.5-11.6 1.5-11.6-4.6V661.9z"
+                fill="#67B47A" p-id="2517"></path>
+            </svg>
             run
           </div>
         </div>
@@ -301,15 +306,14 @@ watch(displayedCode, () => {
         <div class="right-header">
           <h2>Debug Log:</h2>
         </div>
-       <div class="code-print">
-         <div class="code-light code-light-top">
-           <div v-html="err" style="color: red"></div>
-         </div>
-         <div class="code-light">
-           <highlightjs language="Xml" :code="displayedCode">
-           </highlightjs>
-         </div>
-       </div>
+        <div class="code-print">
+          <div class="code-light code-light-top">
+            <div v-html="err" style="color: red"></div>
+          </div>
+          <div class="code-light">
+            <highlightjs language="Xml" :code="displayedCode"/>
+          </div>
+        </div>
       </div>
     </div>
     <div class="console-container">
@@ -318,7 +322,12 @@ watch(displayedCode, () => {
         <div class="console-header" style="display: flex;justify-content: space-between;align-items: center">
           Input Log
           <div class="button-item console-button" @click="logMessage">
-            <svg t="1736214728261" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3688" width="28" height="28"><path d="M715.872 473.392c-15.472-112.736-103.616-196.864-207.36-196.864-103.68 0-191.872 84.064-207.344 196.864h414.72z m-463.072 0l0.192-1.712c16.72-138.08 125.376-243.152 255.52-243.152 130.752 0 239.728 105.984 255.744 244.864h45.488a16 16 0 0 1 16 16v16a16 16 0 0 1-16 16H208a16 16 0 0 1-16-16v-16a16 16 0 0 1 16-16h44.8z m486.656 117.92c13.888 3.472 12.352 13.472 10.912 18.208-34.8 113.696-131.088 192.912-242.08 192.912-110.144 0-205.856-78.016-241.312-190.432-1.536-4.896-4.64-14.432 10.832-18.848l16.528-4.32c15.52-3.968 17.392 5.52 18.864 10.096 29.728 92.56 107.376 155.504 195.088 155.504 88 0 165.824-63.36 195.36-156.304 1.536-4.864 3.616-14.88 19.936-10.832l15.872 4.032z m54.624 84.16h16a16 16 0 0 1 16 16v125.568a16 16 0 0 1-16 16h-125.552a16 16 0 0 1-16-16v-16a16 16 0 0 1 16-16h93.552v-93.568a16 16 0 0 1 16-16z m-570.08 0a16 16 0 0 1 16 16v93.568h93.568a16 16 0 0 1 16 16v16a16 16 0 0 1-16 16H208a16 16 0 0 1-16-16v-125.568a16 16 0 0 1 16-16h16z m570.08-325.92a16 16 0 0 1-16-16V240h-93.552a16 16 0 0 1-16-16v-16a16 16 0 0 1 16-16h125.552a16 16 0 0 1 16 16v125.568a16 16 0 0 1-16 16h-16z m-570.08 0h-16a16 16 0 0 1-16-16V208a16 16 0 0 1 16-16h125.568a16 16 0 0 1 16 16v16a16 16 0 0 1-16 16H240v93.568a16 16 0 0 1-16 16zM591.168 648.32c10.224 11.68 1.04 18.976-3.84 21.648-21.648 11.824-49.536 18.384-78.96 18.384-28.912 0-56.368-6.336-77.872-17.792-5.12-2.736-14.08-9.36-4.32-22.336l10.528-14.016c7.824-11.088 16.048-5.984 20.736-3.84 13.648 6.224 31.584 9.984 50.928 9.984 18.928 0 36.496-3.6 50-9.568 5.168-2.288 12.512-7.664 22.816 4.384l9.984 13.152z" fill="#fff" p-id="3689"></path></svg>
+            <svg t="1736214728261" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                 p-id="3688" width="28" height="28">
+              <path
+                d="M715.872 473.392c-15.472-112.736-103.616-196.864-207.36-196.864-103.68 0-191.872 84.064-207.344 196.864h414.72z m-463.072 0l0.192-1.712c16.72-138.08 125.376-243.152 255.52-243.152 130.752 0 239.728 105.984 255.744 244.864h45.488a16 16 0 0 1 16 16v16a16 16 0 0 1-16 16H208a16 16 0 0 1-16-16v-16a16 16 0 0 1 16-16h44.8z m486.656 117.92c13.888 3.472 12.352 13.472 10.912 18.208-34.8 113.696-131.088 192.912-242.08 192.912-110.144 0-205.856-78.016-241.312-190.432-1.536-4.896-4.64-14.432 10.832-18.848l16.528-4.32c15.52-3.968 17.392 5.52 18.864 10.096 29.728 92.56 107.376 155.504 195.088 155.504 88 0 165.824-63.36 195.36-156.304 1.536-4.864 3.616-14.88 19.936-10.832l15.872 4.032z m54.624 84.16h16a16 16 0 0 1 16 16v125.568a16 16 0 0 1-16 16h-125.552a16 16 0 0 1-16-16v-16a16 16 0 0 1 16-16h93.552v-93.568a16 16 0 0 1 16-16z m-570.08 0a16 16 0 0 1 16 16v93.568h93.568a16 16 0 0 1 16 16v16a16 16 0 0 1-16 16H208a16 16 0 0 1-16-16v-125.568a16 16 0 0 1 16-16h16z m570.08-325.92a16 16 0 0 1-16-16V240h-93.552a16 16 0 0 1-16-16v-16a16 16 0 0 1 16-16h125.552a16 16 0 0 1 16 16v125.568a16 16 0 0 1-16 16h-16z m-570.08 0h-16a16 16 0 0 1-16-16V208a16 16 0 0 1 16-16h125.568a16 16 0 0 1 16 16v16a16 16 0 0 1-16 16H240v93.568a16 16 0 0 1-16 16zM591.168 648.32c10.224 11.68 1.04 18.976-3.84 21.648-21.648 11.824-49.536 18.384-78.96 18.384-28.912 0-56.368-6.336-77.872-17.792-5.12-2.736-14.08-9.36-4.32-22.336l10.528-14.016c7.824-11.088 16.048-5.984 20.736-3.84 13.648 6.224 31.584 9.984 50.928 9.984 18.928 0 36.496-3.6 50-9.568 5.168-2.288 12.512-7.664 22.816 4.384l9.984 13.152z"
+                fill="#fff" p-id="3689"></path>
+            </svg>
             Test
           </div>
         </div>
@@ -346,17 +355,17 @@ watch(displayedCode, () => {
         <div class="progress-item">
           <div class="title">功能覆盖率</div>
           <div class="desc">code coverage</div>
-          <CircularProgress :progress="68"/>
+          <CircularProgress :progress="68" />
         </div>
         <div class="progress-item">
           <div class="title">功能覆盖率</div>
           <div class="desc">code coverage</div>
-          <CircularProgress :progress="88"/>
+          <CircularProgress :progress="88" />
         </div>
         <div class="progress-item">
           <div class="title">功能覆盖率</div>
           <div class="desc">code coverage</div>
-          <CircularProgress :progress="68"/>
+          <CircularProgress :progress="68" />
         </div>
       </div>
     </div>
@@ -458,7 +467,6 @@ watch(displayedCode, () => {
       overflow: hidden;
 
       .right-header {
-        position: absolute;
         padding-left: 20px;
         width: 50%;
       }
@@ -510,26 +518,30 @@ watch(displayedCode, () => {
   display: flex;
   justify-content: space-around;
   align-items: center;
+
   .progress-item {
     display: flex;
     flex-direction: column;
     justify-content: center;
   }
 }
-.button-container{
+
+.button-container {
   display: flex;
   background: #ffffff;
   justify-content: center;
   font-size: 18px;
   height: 32px;
   line-height: 32px;
-  .button-item{
+
+  .button-item {
     display: flex;
     justify-content: center;
     align-items: center;
   }
 }
-.run-btn{
+
+.run-btn {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -541,7 +553,8 @@ watch(displayedCode, () => {
   margin-bottom: 15px;
   padding: 5px 8px;
 }
-.response-params{
+
+.response-params {
   margin-top: 45px;
   color: #fff;
   line-height: 40px;
@@ -612,6 +625,7 @@ input {
   border-radius: 4px;
   color: #fff;
   background-color: #333;
+
   &:focus {
     outline: none;
   }
@@ -647,6 +661,7 @@ button:hover {
 .no-log {
   color: #888;
 }
+
 /* 不同类型的日志 */
 .info {
   color: #7ec8e3;
@@ -659,22 +674,26 @@ button:hover {
 .error {
   color: #f87171;
 }
-.console-button{
+
+.console-button {
   color: #ffffff;
   line-height: 20px;
   background: #0071e3;
   border-radius: 25px;
   padding: 5px 20px;
-  .icon{
+
+  .icon {
     display: inline-block;
   }
 }
-.button-item{
+
+.button-item {
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: pointer;
 }
+
 /* 自定义滚动条样式 */
 .log-list, .console-output {
   scrollbar-width: thin;
@@ -701,14 +720,13 @@ button:hover {
 }
 
 ///* 代码块样式 */
-.code-light-top{
-  margin-top: 45px;
+.code-light-top {
   line-height: 21px;
 }
+
 pre {
   background-color: #2d2d2d;
   color: #f8f8f2;
-  padding: 20px;
   border-radius: 4px;
   font-size: 16px;
   line-height: 1.5;
@@ -718,8 +736,11 @@ code {
   display: block;
   padding: 10px;
 }
+
 .code-print {
-  max-height: 600px;
+  height: calc(100% - 44px);
+  border-radius: 15px;
+  background-color: #2d2d2d;
   width: 100%;
   white-space: wrap;
   overflow-y: auto;
